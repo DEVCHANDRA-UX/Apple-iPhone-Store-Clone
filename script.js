cb4f8b1c0ec1fe63ccae732134597eb4c9bbf7a2
Dev Chandra <sc1614458@gmail.com>
2026-08-01 19:06:56 +0530

    sticky navbar

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ window.addEventListener("load", function(){
     document.querySelector(".loader").classList.add("hide");
 
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+
+        header.style.boxShadow = "0 5px 20px rgba(0,0,0,.1)";
+
+    }
+
+    else{
+
+        header.style.boxShadow = "none";
+
+    }
+
+});
